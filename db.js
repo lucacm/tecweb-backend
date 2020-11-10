cb96a0a0d5ca7f43396c6c057588732277677ad6
William Silva <william.silva.ismart@gmail.com>
2020-11-10 11:21:50 -0300

    alterando algumas configurações pro heroku

--- db.js
+++ db.js
@@ -1,11 +1,18 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/projeto2');
+console.log(process.env.MONGO_CONNECTION);
 
-var userSchema = new mongoose.Schema({
-    user: String,
-    senha: String
-}, { collection: 'usercollection' }
+const uri =
+	process.env.MONGO_CONNECTION || "mongodb://localhost:27017/projeto2";
+
+mongoose.connect(uri);
+
+var userSchema = new mongoose.Schema(
+	{
+		user: String,
+		senha: String,
+	},
+	{ collection: "usercollection" }
 );
 
-module.exports = { Mongoose: mongoose, UserSchema: userSchema }
+module.exports = { Mongoose: mongoose, UserSchema: userSchema };
