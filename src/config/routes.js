72d2b42a3b06ecd712ec092fa5e539052757c59d
William Silva <william.silva.ismart@gmail.com>
2020-11-10 09:46:58 -0300

    k

---
+++ src/config/routes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+module.exports = (server) => {
+	// Rotas da API
+	const router = express.Router();
+	server.use(router);
+	router.get("/", (req, res, next) =>
+		res.status(200).json({ message: "API funcionando /o/" })
+	);
+};
